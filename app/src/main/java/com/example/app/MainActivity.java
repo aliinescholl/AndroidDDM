b667dc5f11a1e8910c8c3f4f2cebe193aa6d15d8
aliinescholl <aline.scholl13@gmail.com>
2025-12-03 00:56:53 -0300

    Implementado o sensor

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.app;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
+    LocationManager lm;
+    TextView tvLatitude, tvLongitude;
+    Button bnt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        bnt=findViewById(R.id.button);
+        lm=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        bnt.setOnClickListener(v->getLocalizcao());
+        tvLatitude=findViewById(R.id.tvLatitude);
+        tvLongitude=findViewById(R.id.tvLongitude);
+    }
+
+    public void getLocalizcao() {
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) && (
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED)
+
+        ) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+
+        Location location;
+        location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location!=null){
+            tvLatitude.setText(Double.toString( location.getLatitude()));
+            tvLongitude.setText(Double.toString( location.getLongitude()));
+        } else{
+            tvLatitude.setText(  "erro" );
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if ( requestCode==1){
+            getLocalizcao();
+        }
     }
 }
