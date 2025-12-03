2e7d763bbcacf8502b6395e684ed2269cc784d93
aliinescholl <aline.scholl13@gmail.com>
2025-12-02 23:35:05 -0300

    Implementado simple list View

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    String[] values;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        listView = findViewById(R.id.listView);
+        values = new String[] { "Android", "iPhone", "WindowsMobile"};
+        adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.listViewValue, values);
+        listView.setAdapter(adapter);
     }
 }
